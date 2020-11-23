55727bde993fc062d6a8d8e6063c1951bacc86b2
Dark-Gran <filip.randak@gmail.com>
2020-11-24 00:59:09 +0100

    Coords helpers

---
+++ core/src/com/darkgran/farstar/util/SimpleBox2.java
@@ -0,0 +1,14 @@
+package com.darkgran.farstar.util;
+
+public class SimpleBox2 extends SimpleVector2 {
+    private float width;
+    private float height;
+
+    public float getWidth() { return width; }
+
+    public void setWidth(float width) { this.width = width; }
+
+    public float getHeight() { return height; }
+
+    public void setHeight(float height) { this.height = height; }
+}
---
+++ core/src/com/darkgran/farstar/util/SimpleVector2.java
@@ -0,0 +1,15 @@
+package com.darkgran.farstar.util;
+
+public class SimpleVector2 {
+    private float x;
+    private float y;
+
+    public float getX() { return x; }
+
+    public void setX(float x) { this.x = x; }
+
+    public float getY() { return y; }
+
+    public void setY(float y) { this.y = y; }
+
+}
