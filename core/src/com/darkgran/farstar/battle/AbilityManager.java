42931ab999d4651f192a603e2c069ac98f5f69b4
Dark-Gran <filip.randak@gmail.com>
2020-12-10 15:48:25 +0100

    todo

--- core/src/com/darkgran/farstar/battle/AbilityManager.java
+++ core/src/com/darkgran/farstar/battle/AbilityManager.java
@@ -9,7 +9,7 @@ import java.util.ListIterator;
 import static com.darkgran.farstar.battle.players.abilities.EffectTypeSpecifics.ChangeStatType.DEFENSE_TYPE;
 import static com.darkgran.farstar.battle.players.abilities.EffectTypeSpecifics.ChangeStatType.OFFENSE_TYPE;
 
-public class AbilityManager { //TODO 1. first strike 2. guard 3. reach
+public class AbilityManager { //TODO 1. guard 2. reach
     private final Battle battle;
 
     public AbilityManager(Battle battle) {
