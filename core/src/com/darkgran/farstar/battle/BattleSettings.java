b2af7b4fb490657609aebc181ae49e169a0d551d
Dark-Gran <filip.randak@gmail.com>
2020-12-13 00:13:56 +0100

    prototype match set-up

--- core/src/com/darkgran/farstar/battle/BattleSettings.java
+++ core/src/com/darkgran/farstar/battle/BattleSettings.java
@@ -9,7 +9,7 @@ public interface BattleSettings {
     int STARTING_CARDS_ATT = 1;
     int STARTING_CARDS_DEF = 2;
     int CARDS_PER_TURN = 1;
-    int MAX_TECH_INCOME = 6;
+    int MAX_TECH_INCOME = 5;
     int MAX_IN_HAND = 8;
     int DECK_SIZE = 30;
     int YARD_SIZE = 5;
