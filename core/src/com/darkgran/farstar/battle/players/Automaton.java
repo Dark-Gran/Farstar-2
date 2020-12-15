748634e01949e0d36b7ff7c67177b836fb620b14
Dark-Gran <filip.randak@gmail.com>
2020-12-15 23:12:08 +0100

    first auto deploy

--- core/src/com/darkgran/farstar/battle/players/Automaton.java
+++ core/src/com/darkgran/farstar/battle/players/Automaton.java
@@ -3,17 +3,23 @@ package com.darkgran.farstar.battle.players;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.Timer;
 import com.darkgran.farstar.battle.Battle;
+import com.darkgran.farstar.battle.gui.CardListMenu;
+import com.darkgran.farstar.battle.gui.DropTarget;
+import com.darkgran.farstar.battle.gui.SupportMenu;
+import com.darkgran.farstar.battle.gui.tokens.Token;
+import com.darkgran.farstar.battle.players.cards.Card;
+import com.darkgran.farstar.battle.players.cards.CardType;
 import com.darkgran.farstar.battle.players.cards.Mothership;
 
-public class Automaton extends Player { //TODO
+public class Automaton extends Player {
     private final BotTier botTier;
     private Battle battle;
-    private float timerDelay = 1.5f;
+    private float timerDelay = 1f;
 
     public Automaton(byte battleID, int energy, int matter, Mothership ms, Deck deck, Yard yard, BotTier botTier) {
         super(battleID, energy, matter, ms, deck, yard);
         this.botTier = botTier;
-        report("Hello World!");
+        report("Hello Universe!");
     }
 
     public void newTurn() {
@@ -23,9 +29,21 @@ public class Automaton extends Player { //TODO
         }, timerDelay);
     }
 
-    public void turn() {
-        report("No idea what to do yet!");
-        battle.getRoundManager().endTurn();
+    private void turn() {
+        deploy(getYard().getCards().get(0), getYard().getCardListMenu());
+        //battle.getRoundManager().endTurn();
+    }
+
+    private void deploy(Card card, CardListMenu cardListMenu) {
+        Token token = new Token(card, getFleet().getFleetMenu().getX(), getFleet().getFleetMenu().getY(), getHand().getCardListMenu().getBattleStage(), cardListMenu);        CardType cardType = token.getCard().getCardInfo().getCardType();
+        DropTarget dropTarget;
+        if (cardType == CardType.SUPPORT) {
+            dropTarget = (SupportMenu) getSupports().getCardListMenu();
+        } else {
+            dropTarget = getFleet().getFleetMenu() ;
+        }
+        int position = getHand().getCardListMenu().getBattleStage().getRoundDropPosition(dropTarget.getSimpleBox2().getX()+dropTarget.getSimpleBox2().getWidth()/2-1, dropTarget.getSimpleBox2().getY()+1, dropTarget, cardType);
+        battle.getRoundManager().processDrop(token, getFleet().getFleetMenu(), position, false, true);
     }
 
     public void newCombat() {
