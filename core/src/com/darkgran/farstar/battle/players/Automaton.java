68f5e6c1dbd6208ca09524491a76c65f59b8d274
Dark-Gran <filip.randak@gmail.com>
2020-12-15 02:06:45 +0100

    timer

--- core/src/com/darkgran/farstar/battle/players/Automaton.java
+++ core/src/com/darkgran/farstar/battle/players/Automaton.java
@@ -1,11 +1,14 @@
 package com.darkgran.farstar.battle.players;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.Timer;
 import com.darkgran.farstar.battle.Battle;
 import com.darkgran.farstar.battle.players.cards.Mothership;
 
 public class Automaton extends Player { //TODO
     private final BotTier botTier;
     private Battle battle;
+    private float timerDelay = 1.5f;
 
     public Automaton(byte battleID, int energy, int matter, Mothership ms, Deck deck, Yard yard, BotTier botTier) {
         super(battleID, energy, matter, ms, deck, yard);
@@ -15,6 +18,12 @@ public class Automaton extends Player { //TODO
 
     public void newTurn() {
         report("My Turn Began!");
+        Timer.schedule(new Timer.Task(){
+            public void run () { Gdx.app.postRunnable(() -> { turn(); }); }
+        }, timerDelay);
+    }
+
+    public void turn() {
         report("No idea what to do yet!");
         battle.getRoundManager().endTurn();
     }
