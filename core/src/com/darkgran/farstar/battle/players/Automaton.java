9500919927dd7bdfd8a4ae67b834ca2a6efbf5fb
Dark-Gran <filip.randak@gmail.com>
2020-12-16 17:43:33 +0100

    todo

--- core/src/com/darkgran/farstar/battle/players/Automaton.java
+++ core/src/com/darkgran/farstar/battle/players/Automaton.java
@@ -7,7 +7,7 @@ import com.darkgran.farstar.battle.players.cards.Mothership;
  *  -- No sensors beyond PossibilityAdvisor
  *  -- No planning
  */
-public class Automaton extends Bot {
+public class Automaton extends Bot { //TODO
 
     public Automaton(byte battleID, int energy, int matter, Mothership ms, Deck deck, Yard yard, BotTier botTier) {
         super(battleID, energy, matter, ms, deck, yard, botTier);
