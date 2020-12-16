85a58c824758c64a6c26928cc7a9b4630d606b35
Dark-Gran <filip.randak@gmail.com>
2020-12-16 20:01:06 +0100

    removed "Constant Interface AntiPattern"

--- core/src/com/darkgran/farstar/battle/BattleSettings.java
+++ core/src/com/darkgran/farstar/battle/BattleSettings.java
@@ -3,15 +3,15 @@ package com.darkgran.farstar.battle;
 public final class BattleSettings {
     private BattleSettings() { }
     //Cards.json setup
-    public static int BONUS_CARD_ID = 21;
+    public static final int BONUS_CARD_ID = 21;
     //Battle
-    public static int STARTING_ENERGY = 1;
-    public static int STARTING_MATTER = 2;
-    public static int STARTING_CARDS_ATT = 1;
-    public static int STARTING_CARDS_DEF = 2;
-    public static int CARDS_PER_TURN = 1;
-    public static int MAX_TECH_INCOME = 5;
-    public static int MAX_IN_HAND = 8;
-    public static int DECK_SIZE = 30;
-    public static int YARD_SIZE = 5;
+    public static final int STARTING_ENERGY = 1;
+    public static final int STARTING_MATTER = 2;
+    public static final int STARTING_CARDS_ATT = 1;
+    public static final int STARTING_CARDS_DEF = 2;
+    public static final int CARDS_PER_TURN = 1;
+    public static final int MAX_TECH_INCOME = 5;
+    public static final int MAX_IN_HAND = 8;
+    public static final int DECK_SIZE = 30;
+    public static final int YARD_SIZE = 5;
 }
