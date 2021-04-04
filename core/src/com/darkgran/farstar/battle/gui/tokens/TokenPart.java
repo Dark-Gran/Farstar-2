f2e4aa0dca94138dff24ab0b213e34be10ae25c9
Dark-Gran <filip.randak@gmail.com>
2021-04-04 22:20:58 +0200

    TextDrawer class => interface

--- core/src/com/darkgran/farstar/battle/gui/tokens/TokenPart.java
+++ core/src/com/darkgran/farstar/battle/gui/tokens/TokenPart.java
@@ -2,6 +2,6 @@ package com.darkgran.farstar.battle.gui.tokens;
 
 import com.darkgran.farstar.util.TextDrawer;
 
-public abstract class TokenPart extends TextDrawer {
+public abstract class TokenPart implements TextDrawer {
 
 }
--- core/src/com/darkgran/farstar/util/TextDrawer.java
+++ core/src/com/darkgran/farstar/util/TextDrawer.java
@@ -4,13 +4,13 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
-public class TextDrawer {
+public interface TextDrawer {
 
-    public void draw(BitmapFont font, Batch batch, float x, float y, String txt) {
+    default void draw(BitmapFont font, Batch batch, float x, float y, String txt) {
         font.draw(batch, txt, x, y);
     }
 
-    public void draw (BitmapFont font, Batch batch, float x, float y, String txt, Color color) {
+    default void draw (BitmapFont font, Batch batch, float x, float y, String txt, Color color) {
         font.setColor(color);
         font.draw(batch, txt, x, y);
     }
