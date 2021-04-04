88f53d94285ee525cd86262ff08cce7f4135c82d
Dark-Gran <filip.randak@gmail.com>
2021-04-05 01:39:25 +0200

    TextDrawer "main draw()" mandatory to override

--- core/src/com/darkgran/farstar/battle/gui/tokens/TokenPart.java
+++ core/src/com/darkgran/farstar/battle/gui/tokens/TokenPart.java
@@ -1,7 +1,9 @@
 package com.darkgran.farstar.battle.gui.tokens;
 
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.darkgran.farstar.util.TextDrawer;
 
-public abstract class TokenPart implements TextDrawer {
-
+public class TokenPart implements TextDrawer {
+    @Override
+    public void draw(Batch batch) { }
 }
