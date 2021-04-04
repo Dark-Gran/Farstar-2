f5be0fa09cdf3f36b34c43943dba772ce47e697c
Dark-Gran <filip.randak@gmail.com>
2021-04-04 21:46:47 +0200

    main menu - game logo

--- core/src/com/darkgran/farstar/MainScreen.java
+++ core/src/com/darkgran/farstar/MainScreen.java
@@ -1,9 +1,12 @@
 package com.darkgran.farstar;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 
 public class MainScreen extends SuperScreen {
     private final MainScreenStage menu = new MainScreenStage(getGame(), getViewport());
+    private final Texture FSLogo = new Texture("images/FSLogo.png");
 
     public MainScreen(final Farstar game, TableStage tableMenu) {
         super(game);
@@ -11,6 +14,11 @@ public class MainScreen extends SuperScreen {
         game.loadLibrary();
         game.getInputMultiplexer().addProcessor(menu);
         Gdx.input.setCursorCatched(false);
+    }
+
+    @Override
+    protected void drawContent(float delta, Batch batch) {
+        batch.draw(FSLogo, Farstar.STAGE_WIDTH/2f-FSLogo.getWidth()/2f, Farstar.STAGE_HEIGHT*0.78f);
     }
 
     @Override
@@ -23,5 +31,6 @@ public class MainScreen extends SuperScreen {
     public void dispose() {
         getGame().getInputMultiplexer().removeProcessor(menu);
         menu.dispose();
+        FSLogo.dispose();
     }
 }
