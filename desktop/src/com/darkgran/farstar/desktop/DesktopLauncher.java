c2cca2e516c274f63f574c09b05018f079486d19
Dark-Gran <filip.randak@gmail.com>
2021-04-05 13:18:17 +0200

    disabled vSync

--- desktop/src/com/darkgran/farstar/desktop/DesktopLauncher.java
+++ desktop/src/com/darkgran/farstar/desktop/DesktopLauncher.java
@@ -13,6 +13,7 @@ public class DesktopLauncher {
 		config.foregroundFPS = 60;
 		config.resizable = true;
 		config.fullscreen = false;
+		config.vSyncEnabled = false;
 		new LwjglApplication(new Farstar(), config);
 	}
 }
