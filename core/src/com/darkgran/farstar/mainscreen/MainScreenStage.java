546a5f0ab19f8dd45cf6da762b3d9e81514fa29f
Dark-Gran <filip.randak@gmail.com>
2021-04-06 13:51:51 +0200

    SimpleButton to replace box2d.ImageButton

--- core/src/com/darkgran/farstar/mainscreen/MainScreenStage.java
+++ core/src/com/darkgran/farstar/mainscreen/MainScreenStage.java
@@ -2,17 +2,13 @@ package com.darkgran.farstar.mainscreen;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.darkgran.farstar.Farstar;
 import com.darkgran.farstar.ListeningStage;
 import com.darkgran.farstar.battle.Battle1v1;
 import com.darkgran.farstar.battle.BattleScreen;
 import com.darkgran.farstar.battle.players.PlayerFactory;
+import com.darkgran.farstar.util.SimpleButton;
 
 public class MainScreenStage extends ListeningStage {
     private final PlayerFactory playerFactory = new PlayerFactory();
@@ -20,13 +16,37 @@ public class MainScreenStage extends ListeningStage {
     private final Texture skirmish = new Texture("images/skirmish.png");
     private final Texture sim = new Texture("images/sim.png");
     private final Texture web = new Texture("images/web.png");
-    private final ImageButton startButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(solitary)));
-    private final ImageButton botButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(skirmish)));
-    private final ImageButton simButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(sim)));
-    private final ImageButton webButton = new ImageButton(new TextureRegionDrawable((new TextureRegion(web))));
     private final Texture FSLogo = new Texture("images/FSLogo.png");
     private final VersionInfo versionInfo = new VersionInfo((float) (Farstar.STAGE_WIDTH*0.85), (float) (Farstar.STAGE_HEIGHT*0.98), new Color(0.31f, 0.498f, 0.706f, 1));
     private final PerfMeter perfMeter = new PerfMeter((float) (Farstar.STAGE_WIDTH*0.085), (float) (Farstar.STAGE_HEIGHT*0.98), new Color(0.31f, 0.498f, 0.706f, 1));
+
+    private final SimpleButton startButton = new SimpleButton(solitary, solitary){
+        @Override
+        public void clicked() {
+            System.out.println("Starting Solitary.");
+            getGame().setScreen(new BattleScreen(getGame(), getGame().getSuperScreen().getTableMenu(), new Battle1v1(playerFactory.getPlayer("LOCAL", 1, 0), playerFactory.getPlayer("LOCAL", 2, 15))));
+        }
+    };
+    private final SimpleButton botButton = new SimpleButton(skirmish, skirmish){
+        @Override
+        public void clicked() {
+            System.out.println("Starting Skirmish.");
+            getGame().setScreen(new BattleScreen(getGame(), getGame().getSuperScreen().getTableMenu(), new Battle1v1(playerFactory.getPlayer("LOCAL", 1, 0), playerFactory.getPlayer("AUTO", 2, 15))));
+        }
+    };
+    private final SimpleButton simButton = new SimpleButton(sim, sim){
+        @Override
+        public void clicked() {
+            System.out.println("Starting Simulation.");
+            getGame().setScreen(new BattleScreen(getGame(), getGame().getSuperScreen().getTableMenu(), new Battle1v1(playerFactory.getPlayer("AUTO", 1, 0), playerFactory.getPlayer("AUTO", 2, 15))));
+        }
+    };
+    private final SimpleButton webButton = new SimpleButton(web, web){
+        @Override
+        public void clicked() {
+            System.out.println("LinkButton Click"); //TODO
+        }
+    };
 
     public MainScreenStage(final Farstar game, Viewport viewport) {
         super(game, viewport);
@@ -52,50 +72,10 @@ public class MainScreenStage extends ListeningStage {
     }
 
     @Override
-    protected void setupListeners() { //in-future: separate method for "battle start", as there will be many more buttons
-        startButton.addListener(new ClickListener()
-        {
-            @Override
-            public void clicked(InputEvent event, float x, float y)
-            {
-                System.out.println("Starting Solitary.");
-                getGame().setScreen(new BattleScreen(getGame(), getGame().getSuperScreen().getTableMenu(), new Battle1v1(playerFactory.getPlayer("LOCAL", 1, 0), playerFactory.getPlayer("LOCAL", 2, 15))));
-            }
-        });
-        botButton.addListener(new ClickListener()
-        {
-            @Override
-            public void clicked(InputEvent event, float x, float y)
-            {
-                System.out.println("Starting Skirmish.");
-                getGame().setScreen(new BattleScreen(getGame(), getGame().getSuperScreen().getTableMenu(), new Battle1v1(playerFactory.getPlayer("LOCAL", 1, 0), playerFactory.getPlayer("AUTO", 2, 15))));
-            }
-        });
-        simButton.addListener(new ClickListener()
-        {
-            @Override
-            public void clicked(InputEvent event, float x, float y)
-            {
-                System.out.println("Starting Simulation.");
-                getGame().setScreen(new BattleScreen(getGame(), getGame().getSuperScreen().getTableMenu(), new Battle1v1(playerFactory.getPlayer("AUTO", 1, 0), playerFactory.getPlayer("AUTO", 2, 15))));
-            }
-        });
-        webButton.addListener(new ClickListener()
-        {
-            @Override
-            public void clicked(InputEvent event, float x, float y)
-            {
-                System.out.println("LinkButton Click"); //TODO
-            }
-        });
-    }
+    protected void setupListeners() { }
 
     @Override
     public void dispose() {
-        startButton.removeListener(startButton.getClickListener());
-        botButton.removeListener(botButton.getClickListener());
-        simButton.removeListener(simButton.getClickListener());
-        webButton.removeListener(webButton.getClickListener());
         solitary.dispose();
         sim.dispose();
         skirmish.dispose();
---
+++ core/src/com/darkgran/farstar/util/SimpleButton.java
@@ -0,0 +1,40 @@
+package com.darkgran.farstar.util;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+
+public class SimpleButton extends Actor {
+    private final Texture imageUp;
+    private final Texture imageOver;
+    private final ClickListener clickListener = new ClickListener(){
+        @Override
+        public void clicked(InputEvent event, float x, float y)
+        {
+            SimpleButton.this.clicked();
+        }
+    };
+
+    public SimpleButton(Texture imageUp, Texture imageOver) {
+        this.imageUp = imageUp;
+        this.imageOver = imageOver;
+        setWidth(imageOver.getWidth());
+        setHeight(imageOver.getHeight());
+        addListener(clickListener);
+    }
+
+    public void clicked() { }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        if (clickListener.isOver()) {
+            batch.draw(imageOver, getX(), getY());
+        } else {
+            batch.draw(imageUp, getX(), getY());
+        }
+    }
+
+}
