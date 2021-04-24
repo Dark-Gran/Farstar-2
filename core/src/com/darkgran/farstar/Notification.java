2b2a82cf35af21ebb4facdaeb17db67c6ffb4ce2
Dark-Gran <filip.randak@gmail.com>
2021-04-24 16:06:30 +0200

    TextInTheBox as a parent class for all "message boxes"

--- core/src/com/darkgran/farstar/Notification.java
+++ core/src/com/darkgran/farstar/Notification.java
@@ -6,16 +6,13 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.darkgran.farstar.util.DeltaCounter;
-import com.darkgran.farstar.util.JustFont;
-import com.darkgran.farstar.util.SimpleCounter;
-import com.darkgran.farstar.util.TextDrawer;
+import com.darkgran.farstar.util.*;
 
 /**
  *  Stylized on-screen message.
  *  Uses NotificationType for placement.
  */
-public class Notification implements TextDrawer, JustFont {
+public class Notification extends TextInTheBox {
     public enum NotificationType {
         BOT_LEFT(Farstar.STAGE_WIDTH/10f, Farstar.STAGE_HEIGHT/5f);
 
@@ -29,21 +26,16 @@ public class Notification implements TextDrawer, JustFont {
     }
 
     private static final int MIN_DURATION = 3;
-    private String fontPath = "";
-    private Color fontColor = new Color();
-    private String message = "";
     private final NotificationType notificationType;
     private final GlyphLayout layout = new GlyphLayout();
-    private final Color boxColor = new Color(ColorPalette.DARK.r, ColorPalette.DARK.g, ColorPalette.DARK.b, 0.5f);
     private final DeltaCounter timer;
 
-    /** @param duration Time in seconds. Set to MIN_DURATION(=3) unless greater duration is provided. */
+    /** @param duration Time in seconds. Set to MIN_DURATION unless greater duration is provided. */
     protected Notification(NotificationType notificationType, String message, int duration) {
-        setFont("fonts/barlow30.fnt");
-        setFontColor(ColorPalette.LIGHT);
-        this.message = message;
+        super(ColorPalette.LIGHT, new Color(ColorPalette.DARK.r, ColorPalette.DARK.g, ColorPalette.DARK.b, 0.5f), "fonts/barlow30.fnt", message);
         this.notificationType = notificationType;
         layout.setText(getFont(), message);
+        setupBox(notificationType.x, notificationType.y, layout.width, layout.height);
         timer = new DeltaCounter(true, Math.max(duration, MIN_DURATION), 0);
     }
 
@@ -51,19 +43,20 @@ public class Notification implements TextDrawer, JustFont {
         timer.update(delta);
     }
 
+    @Override
     public void draw(Batch batch, ShapeRenderer shapeRenderer) {
         batch.end();
         Gdx.gl.glEnable(GL20.GL_BLEND);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.setColor(boxColor.r, boxColor.g, boxColor.b, timeToAlpha(boxColor.a, timer.getCount(), timer.getCountCap()));
+        shapeRenderer.setColor(getBoxColor().r, getBoxColor().g, getBoxColor().b, timeToAlpha(getBoxColor().a, timer.getCount(), timer.getCountCap()));
         shapeRenderer.rect(notificationType.x - Farstar.STAGE_WIDTH / 19f, notificationType.y + layout.height / 2, Farstar.STAGE_WIDTH / 3f, -layout.height * 2.1f);
         shapeRenderer.setColor(Color.WHITE);
         shapeRenderer.end();
         Gdx.gl.glDisable(GL20.GL_BLEND);
         batch.begin();
-        Color color = new Color(fontColor.r, fontColor.g, fontColor.b, timeToAlpha(fontColor.a, timer.getCount(), timer.getCountCap()));
-        draw(getFont(), batch, notificationType.x, notificationType.y, message, color);
+        Color color = new Color(getFontColor().r, getFontColor().g, getFontColor().b, timeToAlpha(getFontColor().a, timer.getCount(), timer.getCountCap()));
+        drawText(getFont(), batch, notificationType.x, notificationType.y, getMessage(), color);
     }
 
     private float timeToAlpha(float a, float time, float duration) {
@@ -73,33 +66,6 @@ public class Notification implements TextDrawer, JustFont {
             a *= 1-timer.getAccumulator();
         }
         return a;
-    }
-
-    @Override
-    public void draw(Batch batch) { }
-
-    protected String getMessage() {
-        return message;
-    }
-
-    @Override
-    public String getFontPath() {
-        return fontPath;
-    }
-
-    @Override
-    public void setFontPath(String path) {
-        fontPath = path;
-    }
-
-    @Override
-    public Color getFontColor() {
-        return fontColor;
-    }
-
-    @Override
-    public void setFontColor(Color fontColor) {
-        this.fontColor = fontColor;
     }
 
     public SimpleCounter getTimer() {
---
+++ core/src/com/darkgran/farstar/util/TextInTheBox.java
@@ -0,0 +1,71 @@
+package com.darkgran.farstar.util;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+/**
+ * Text at coordinates with a box around it.
+ */
+public class TextInTheBox extends SimpleBox2 implements TextDrawer, JustFont {
+    private Color fontColor;
+    private Color boxColor;
+    private String fontPath = "";
+    private String message = "";
+
+    public TextInTheBox(Color fontColor, Color boxColor, String fontPath, String message) {
+        setFont(fontPath);
+        setFontColor(fontColor);
+        this.boxColor = boxColor;
+        this.message = message;
+        setupBox(0, 0, 1, 1);
+    }
+
+    /** Box outlines keep the same distance from text on all sides. */
+    public void draw(Batch batch, ShapeRenderer shapeRenderer) {
+        //BOX
+        batch.end();
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(getBoxColor());
+        shapeRenderer.rect(getX()-getHeight()/2, getY()+getHeight()/2, getWidth()+getHeight(), -getHeight()*2);
+        shapeRenderer.setColor(Color.WHITE);
+        shapeRenderer.end();
+        batch.begin();
+        //TEXT
+        drawText(batch);
+    }
+
+    @Override
+    public void drawText(Batch batch) {
+        drawText(getFont(), batch, getX(), getY(), getMessage(), getFontColor());
+    }
+
+    @Override
+    public String getFontPath() {
+        return fontPath;
+    }
+
+    @Override
+    public void setFontPath(String path) {
+        fontPath = path;
+    }
+
+    @Override
+    public Color getFontColor() {
+        return fontColor;
+    }
+
+    @Override
+    public void setFontColor(Color fontColor) {
+        this.fontColor = fontColor;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Color getBoxColor() {
+        return boxColor;
+    }
+
+}
