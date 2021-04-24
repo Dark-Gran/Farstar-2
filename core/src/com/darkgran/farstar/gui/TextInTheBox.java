e839371042f6829f1087eadd4bcbe753cafbd699
Dark-Gran <filip.randak@gmail.com>
2021-04-25 01:37:06 +0200

    centralizeBox()

--- core/src/com/darkgran/farstar/gui/TextInTheBox.java
+++ core/src/com/darkgran/farstar/gui/TextInTheBox.java
@@ -2,36 +2,38 @@ package com.darkgran.farstar.gui;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.darkgran.farstar.util.SimpleBox2;
+import com.darkgran.farstar.util.SimpleVector2;
 
 /**
  * Text at coordinates with a box around it.
  */
 public class TextInTheBox extends TextLine {
     private Color boxColor;
-    private final SimpleBox2 simpleBox;
+    private final SimpleBox2 simpleBox = new SimpleBox2(0, 0, 1, 1);;
     private final boolean noBox;
 
     public TextInTheBox(Color fontColor, Color boxColor, String fontPath, String text) {
         super(fontColor, fontPath, 0, 0, text);
         this.boxColor = boxColor;
         this.noBox = false;
-        simpleBox = new SimpleBox2(0, 0, 1, 1);
     }
 
     public TextInTheBox(Color fontColor, Color boxColor, String fontPath, String text, boolean noBox) {
         super(fontColor, fontPath, 0, 0, text);
         this.boxColor = boxColor;
         this.noBox = noBox;
-        simpleBox = new SimpleBox2(0, 0, 1, 1);
     }
 
     public TextInTheBox(Color fontColor, Color boxColor, String fontPath, String text, float x, float y, float width, float height) {
         super(fontColor, fontPath, x, y, text);
         this.boxColor = boxColor;
         this.noBox = false;
-        simpleBox = new SimpleBox2(x, y, width, height);
+        simpleBox.setWidth(width);
+        simpleBox.setHeight(height);
+        centralizeBox();
     }
 
     public TextInTheBox(Color fontColor, Color boxColor, String fontPath, String text, float x, float y, float width, float height, boolean noBox) {
@@ -40,7 +42,18 @@ public class TextInTheBox extends TextLine {
         setFontColor(fontColor);
         this.boxColor = boxColor;
         this.noBox = noBox;
-        simpleBox = new SimpleBox2(x, y, width, height);
+        simpleBox.setWidth(width);
+        simpleBox.setHeight(height);
+        centralizeBox();
+    }
+
+    public void centralizeBox() {
+        SimpleVector2 boxOrigin = boxOriginFromTextCenter(simpleBox.getWidth(), simpleBox.getHeight(), getFont(), getText(), getX(), getY());
+        setupBox(boxOrigin.getX(), boxOrigin.getY(), simpleBox.getWidth(), simpleBox.getHeight());
+    }
+
+    public static SimpleVector2 boxOriginFromTextCenter(float width, float height, BitmapFont font, String text, float x, float y) {
+        return new SimpleVector2((x + TextDrawer.getTextWH(font, text).getX()/2) - width/2,(y - TextDrawer.getTextWH(font, text).getY()/2) - height/2);
     }
 
     public void setupBox(float x, float y, float width, float height) {
@@ -57,7 +70,7 @@ public class TextInTheBox extends TextLine {
             batch.end();
             shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
             shapeRenderer.setColor(getBoxColor());
-            shapeRenderer.rect(getX() - simpleBox.getHeight() / 2, getY() + simpleBox.getHeight() / 2, simpleBox.getWidth() + simpleBox.getHeight(), -simpleBox.getHeight() * 2);
+            shapeRenderer.rect(simpleBox.getX(), simpleBox.getY(), simpleBox.getWidth(), simpleBox.getHeight());
             shapeRenderer.setColor(Color.WHITE);
             shapeRenderer.end();
             batch.begin();
