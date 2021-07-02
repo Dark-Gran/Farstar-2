1ad8f15f829cf9dad3c5e40d2a93433bb7d30398
Dark-Gran <filip.randak@gmail.com>
2021-07-02 14:49:15 +0200

    textures sorted alphabetically

--- core/src/com/darkgran/farstar/AssetLibrary.java
+++ core/src/com/darkgran/farstar/AssetLibrary.java
@@ -28,35 +28,38 @@ public class AssetLibrary {
         texParams.minFilter = Texture.TextureFilter.Linear;
         texParams.magFilter = Texture.TextureFilter.Nearest;
         //All-Screens
-        assetManager.load("images/y.png", Texture.class, texParams);
-        assetManager.load("images/yO.png", Texture.class, texParams);
         assetManager.load("images/x.png", Texture.class, texParams);
         assetManager.load("images/xO.png", Texture.class, texParams);
+        assetManager.load("images/y.png", Texture.class, texParams);
+        assetManager.load("images/yO.png", Texture.class, texParams);
         //Intro
         assetManager.load("images/logo.jpg", Texture.class, texParams);
         //MainScreenStage
         assetManager.load("images/FSlogo.png", Texture.class, texParams);
-        assetManager.load("images/solitary.png", Texture.class, texParams);
-        assetManager.load("images/solitaryO.png", Texture.class, texParams);
         assetManager.load("images/skirmish.png", Texture.class, texParams);
         assetManager.load("images/skirmishO.png", Texture.class, texParams);
         assetManager.load("images/sim.png", Texture.class, texParams);
         assetManager.load("images/simO.png", Texture.class, texParams);
+        assetManager.load("images/solitary.png", Texture.class, texParams);
+        assetManager.load("images/solitaryO.png", Texture.class, texParams);
         assetManager.load("images/web.png", Texture.class, texParams);
         assetManager.load("images/webO.png", Texture.class, texParams);
         //TableStage
-        assetManager.load("images/tableMain_1920.png", Texture.class, texParams);
-        assetManager.load("images/Space_1920.png", Texture.class, texParams);
         assetManager.load("images/empty.png", Texture.class, texParams);
         assetManager.load("images/exit.png", Texture.class, texParams);
-        assetManager.load("images/quality.png", Texture.class, texParams);
-        assetManager.load("images/fs.png", Texture.class, texParams);
-        assetManager.load("images/sound.png", Texture.class, texParams);
-        assetManager.load("images/logout.png", Texture.class, texParams);
         assetManager.load("images/friends.png", Texture.class, texParams);
+        assetManager.load("images/fs.png", Texture.class, texParams);
+        assetManager.load("images/logout.png", Texture.class, texParams);
+        assetManager.load("images/quality.png", Texture.class, texParams);
+        assetManager.load("images/sound.png", Texture.class, texParams);
+        assetManager.load("images/Space_1920.png", Texture.class, texParams);
+        assetManager.load("images/tableMain_1920.png", Texture.class, texParams);
         //Battle
+        assetManager.load("images/combat_end.png", Texture.class, texParams);
         assetManager.load("images/duel.png", Texture.class, texParams);
         assetManager.load("images/duel_cancel.png", Texture.class, texParams);
+        assetManager.load("images/matter.png", Texture.class, texParams);
+        assetManager.load("images/energy.png", Texture.class, texParams);
         assetManager.load("images/turn.png", Texture.class, texParams);
         assetManager.load("images/turnO.png", Texture.class, texParams);
         assetManager.load("images/turnOP.png", Texture.class, texParams);
@@ -65,9 +68,6 @@ public class AssetLibrary {
         assetManager.load("images/yardO.png", Texture.class, texParams);
         assetManager.load("images/yardOP.png", Texture.class, texParams);
         assetManager.load("images/yardP.png", Texture.class, texParams);
-        assetManager.load("images/combat_end.png", Texture.class, texParams);
-        assetManager.load("images/energy.png", Texture.class, texParams);
-        assetManager.load("images/matter.png", Texture.class, texParams);
     }
 
     public void dispose() {
