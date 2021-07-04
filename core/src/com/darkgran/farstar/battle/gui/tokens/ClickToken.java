41fafdb373013d87feb9c5431f4a07acc6f2d0ff
Dark-Gran <filip.randak@gmail.com>
2021-07-04 21:02:32 +0200

    returned missing touchDown/Up

--- core/src/com/darkgran/farstar/battle/gui/tokens/ClickToken.java
+++ core/src/com/darkgran/farstar/battle/gui/tokens/ClickToken.java
@@ -1,14 +1,23 @@
 package com.darkgran.farstar.battle.gui.tokens;
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.darkgran.farstar.battle.gui.BattleStage;
 import com.darkgran.farstar.battle.gui.CardListMenu;
 import com.darkgran.farstar.battle.players.cards.Card;
 
 public abstract class ClickToken extends Token {
-    private ClickListener clickListener = new ClickListener(){ };
+    private ClickListener clickListener = new ClickListener(){
+        @Override
+        public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+            return true;
+        }
+
+        @Override
+        public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+            click(button);
+        }
+    };
 
     public ClickToken(Card card, float x, float y, BattleStage battleStage, CardListMenu cardListMenu, TokenType tokenType) {
         super(card, x, y, battleStage, cardListMenu, tokenType);
