f5d18870f64127f9b28fc49ad5fd5aa23ed60c0c
Dark-Gran <filip.randak@gmail.com>
2021-07-06 16:44:55 +0200

    changed token sizes to follow same ratio (xcept junk)

--- core/src/com/darkgran/farstar/battle/gui/tokens/TokenType.java
+++ core/src/com/darkgran/farstar/battle/gui/tokens/TokenType.java
@@ -1,12 +1,12 @@
 package com.darkgran.farstar.battle.gui.tokens;
 
 public enum TokenType {
-    MS(340f, 180f),
+    MS(309f, 184f),
     FAKE(260f, 155f),
     FLEET(232f, 138f),
-    YARD(216f, 120f),
-    SUPPORT(190f, 100f),
-    JUNK(88f, 116f);
+    YARD(216f, 128f),
+    SUPPORT(190f, 113f),
+    JUNK(88f, 116f); //to be adjusted
 
     private final float width;
     private final float height;
