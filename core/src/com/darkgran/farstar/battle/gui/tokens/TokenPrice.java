09d0ab91a0b51ea2016d44e3c606e39a3d2da463
Dark-Gran <filip.randak@gmail.com>
2021-07-07 14:48:31 +0200

    tokenPrice double-pad

--- core/src/com/darkgran/farstar/battle/gui/tokens/TokenPrice.java
+++ core/src/com/darkgran/farstar/battle/gui/tokens/TokenPrice.java
@@ -1,18 +1,17 @@
 package com.darkgran.farstar.battle.gui.tokens;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.darkgran.farstar.Farstar;
+import com.darkgran.farstar.gui.TextDrawer;
+import com.darkgran.farstar.util.SimpleVector2;
 
 public class TokenPrice extends TokenPart {
     private Texture pad2;
+    private SimpleVector2 textWH2;
 
     public TokenPrice(String fontPath, Token token) {
         super(fontPath, token);
-    }
-
-    @Override
-    public String getContent() {
-        return getToken().getCard().getCardInfo().getEnergy() + ":" + getToken().getCard().getCardInfo().getMatter();
     }
 
     @Override
@@ -23,8 +22,40 @@ public class TokenPrice extends TokenPart {
 
     @Override
     public void setupOffset() {
-        setOffsetY(-getPad().getHeight());
+        setOffsetY(-1f-getPad().getHeight());
         setOffsetX(getPad().getWidth());
     }
 
+    @Override
+    public void update() {
+        String e = Integer.toString(getToken().getCard().getCardInfo().getEnergy());
+        String m = Integer.toString(getToken().getCard().getCardInfo().getMatter());
+        setTextWH(TextDrawer.getTextWH(getFont(), e));
+        textWH2 = TextDrawer.getTextWH(getFont(), m);
+        if (e.equals("1")) {
+            getTextWH().setX(getTextWH().getX()+3f);
+        }
+        if (m.equals("1")) {
+            textWH2.setX(textWH2.getX()+3f);
+        }
+    }
+
+    @Override
+    public void adjustTextWH() {}
+
+    @Override
+    public void draw(Batch batch) {
+        float x = getX()-getPad().getWidth()+getOffsetX();
+        if (getToken().getCard().getCardInfo().getEnergy() != 0) {
+            String e = Integer.toString(getToken().getCard().getCardInfo().getEnergy());
+            batch.draw(getPad(), x, getY()+getOffsetY());
+            drawText(getFont(), batch, x+getPad().getWidth()*0.5f-getTextWH().getX()*0.5f, getY()+getOffsetY()+getPad().getHeight()*0.5f+ getTextWH().getY()*0.48f, e);
+            x += getPad().getWidth();
+        }
+        if (getToken().getCard().getCardInfo().getMatter() != 0) {
+            String m = Integer.toString(getToken().getCard().getCardInfo().getMatter());
+            batch.draw(pad2, x, getY()+getOffsetY());
+            drawText(getFont(), batch, x+getPad().getWidth()*0.5f-textWH2.getX()*0.5f, getY()+getOffsetY()+getPad().getHeight()*0.5f+ textWH2.getY()*0.48f, m);
+        }
+    }
 }
