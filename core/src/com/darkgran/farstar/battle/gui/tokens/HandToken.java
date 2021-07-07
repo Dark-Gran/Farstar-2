292695d49dad94b3c8cd440112db1c18f4612942
Dark-Gran <filip.randak@gmail.com>
2021-07-07 23:28:43 +0200

    card rotatable (gfx+actor)

--- core/src/com/darkgran/farstar/battle/gui/tokens/HandToken.java
+++ core/src/com/darkgran/farstar/battle/gui/tokens/HandToken.java
@@ -2,14 +2,17 @@ package com.darkgran.farstar.battle.gui.tokens;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.darkgran.farstar.Farstar;
 import com.darkgran.farstar.battle.gui.BattleStage;
 import com.darkgran.farstar.battle.gui.CardListMenu;
 import com.darkgran.farstar.battle.players.cards.Card;
 import com.darkgran.farstar.battle.players.cards.CardType;
+import com.darkgran.farstar.util.SimpleVector2;
 
 public class HandToken extends AnchoredToken {
     public enum HandState {
@@ -20,6 +23,14 @@ public class HandToken extends AnchoredToken {
     private HandState currentState = HandState.DOWN;
     private HandState nextState = currentState;
     private float angle = 0f;
+    Matrix4 oldMX = null;
+    Matrix4 mx;
+    public static SimpleVector2 getNewXYFromAngle(float currentX, float currentY, float centerX, float centerY, double rads) {
+        return new SimpleVector2(
+                (float) ((currentX-centerX) * Math.cos(rads) - (currentY-centerY) * Math.sin(rads) + centerX),
+                (float) ((currentX-centerX) * Math.sin(rads) + (currentY-centerY) * Math.cos(rads) + centerY)
+        );
+    }
 
 
     public HandToken(Card card, float x, float y, BattleStage battleStage, CardListMenu cardListMenu) {
@@ -44,6 +55,8 @@ public class HandToken extends AnchoredToken {
         });
         this.addListener(getDragger());
         cardPic = Farstar.ASSET_LIBRARY.get("images/tokens/card_D.png");
+        setOriginX(getWidth()/2);
+        setOriginY(getHeight()/2);
         refreshRotation();
     }
 
@@ -65,12 +78,18 @@ public class HandToken extends AnchoredToken {
             getTokenOffense().setPad(tokenType);
             getTokenPrice().setPad(tokenType);
             setPosition(getX(), getY());
+            setOriginX(getWidth()/2);
+            setOriginY(getHeight()/2);
             currentState = nextState;
         }
     }
 
     public void refreshRotation() {
         angle = getNewAngle();
+        mx = new Matrix4();
+        mx.rotate(new Vector3(0, 0, 1), angle);
+        SimpleVector2 newXY = getNewXYFromAngle(0, 0, getX()+getWidth()/2, getY()+getHeight()/2, Math.toRadians(angle));
+        mx.trn(newXY.getX(), newXY.getY(), 0);
         setRotation(angle);
     }
 
@@ -83,21 +102,35 @@ public class HandToken extends AnchoredToken {
     @Override
     public void draw(Batch batch) {
         //Rotate
-        Matrix4 oldMX = batch.getTransformMatrix().cpy();
-        Matrix4 mx = new Matrix4();
-        mx.rotate(new Vector3(0, 0, 1), angle);
-        mx.trn(getX(), getY(), 0);
-        batch.setTransformMatrix(mx);
+        if (currentState == HandState.DOWN) {
+            oldMX = batch.getTransformMatrix().cpy();
+            batch.setTransformMatrix(mx);
+        } else if (oldMX != null) {
+            oldMX = null;
+        }
         //Draw
         batch.draw(cardPic, getX(), getY());
         super.draw(batch);
-        batch.setTransformMatrix(oldMX);
+        if (oldMX != null) { batch.setTransformMatrix(oldMX); }
     }
 
     @Override
     protected void drawPortrait(Batch batch) {
         if (getPortrait() != null) { batch.draw(getPortrait(), getX(), getY()+cardPic.getHeight()-getPortrait().getHeight()+PORTRAIT_OFFSET_Y); }
         if (getFrame() != null) { batch.draw(getFrame(), getX(), getY()+cardPic.getHeight()-getFrame().getHeight()+PORTRAIT_OFFSET_Y); }
+    }
+
+    @Override
+    protected void debugRender(Batch batch) {
+        if (currentState == HandState.DOWN) {
+            batch.end();
+            getBattleStage().getBattleScreen().getShapeRenderer().begin(ShapeRenderer.ShapeType.Line);
+            getBattleStage().getBattleScreen().getShapeRenderer().rect(getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(), 1f, 1f, getRotation());
+            getBattleStage().getBattleScreen().getShapeRenderer().end();
+            batch.begin();
+        } else {
+            super.debugRender(batch);
+        }
     }
 
     @Override
