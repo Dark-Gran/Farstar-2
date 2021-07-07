a95272458b7c754a07646da35aca18234fe8173d
Dark-Gran <filip.randak@gmail.com>
2021-07-07 20:55:15 +0200

    HandMenu max width (card covering)

--- core/src/com/darkgran/farstar/battle/gui/HandMenu.java
+++ core/src/com/darkgran/farstar/battle/gui/HandMenu.java
@@ -36,20 +36,41 @@ public class HandMenu extends CardListMenu {
     }
 
     private void centralize() {
-        float offset = TokenType.HAND.getWidth() * getTokens().size() * 0.5f;
+        float covering = 1f;
+        if (getTokens().size() > 4) {
+            switch (getTokens().size()) {
+                case 5:
+                    covering = 0.89f;
+                    break;
+                case 6:
+                    covering = 0.72f;
+                    break;
+                case 7:
+                    covering = 0.61f;
+                    break;
+                case 8:
+                    covering = 0.53f;
+                    break;
+                default:
+                    covering = 1f - (getTokens().size()-4)*0.2f;
+                    break;
+            }
+        }
+        float offset = ((TokenType.HAND.getWidth() * getTokens().size())-TokenType.HAND.getWidth()*(1f-covering)*(getTokens().size()-1)) * 0.5f;
         actualX = getX()-offset;
-        refreshTokenPositions();
+        refreshTokenPositions(covering);
     }
 
-    private void refreshTokenPositions() {
+    private void refreshTokenPositions(float covering) {
         for (int i = 0; i < getTokens().size(); i++) {
-            getTokens().get(i).setPosition(actualX + getOffset()*i, actualY);
+            getTokens().get(i).setPosition(actualX + getOffset()*i*covering, actualY);
+            ((AnchoredToken) getTokens().get(i)).setNewAnchor(actualX + getOffset()*i*covering, actualY);
         }
     }
 
     @Override
     public void setupOffset() {
-        setOffset(TokenType.FLEET.getWidth());
+        setOffset(TokenType.HAND.getWidth());
         if (isNegativeOffset()) { setOffset(getOffset()*-1); }
     }
 
