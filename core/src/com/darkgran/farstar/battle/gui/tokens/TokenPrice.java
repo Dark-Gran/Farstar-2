30e5929e679e1724e07c18fb9a24cb7fbaed0736
Dark-Gran <filip.randak@gmail.com>
2021-07-08 16:39:38 +0200

    Revert "TokenPrice clickListener fix"
    This reverts commit 078a00f1

--- core/src/com/darkgran/farstar/battle/gui/tokens/TokenPrice.java
+++ core/src/com/darkgran/farstar/battle/gui/tokens/TokenPrice.java
@@ -16,8 +16,9 @@ public class TokenPrice extends TokenPart {
     private Texture abiMark;
     private ClickListener clickListener;
 
-    public TokenPrice(String fontPath, Card card, TokenType tokenType, boolean noPics) {
+    public TokenPrice(String fontPath, Card card, TokenType tokenType, boolean noPics, ClickListener clickListener) {
         super(fontPath, card, tokenType, noPics);
+        this.clickListener = clickListener;
     }
 
     @Override
@@ -114,11 +115,4 @@ public class TokenPrice extends TokenPart {
         return false;
     }
 
-    public ClickListener getClickListener() {
-        return clickListener;
-    }
-
-    public void setClickListener(ClickListener clickListener) {
-        this.clickListener = clickListener;
-    }
 }
