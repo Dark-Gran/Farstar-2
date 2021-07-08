7097f715b2f54858029dedd10d564a7df8832325
Dark-Gran <filip.randak@gmail.com>
2021-07-08 23:57:42 +0200

    TokenZoom counter

--- core/src/com/darkgran/farstar/battle/gui/tokens/TokenZoom.java
+++ core/src/com/darkgran/farstar/battle/gui/tokens/TokenZoom.java
@@ -4,22 +4,48 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.darkgran.farstar.battle.gui.BattleStage;
 import com.darkgran.farstar.battle.gui.CardListMenu;
 import com.darkgran.farstar.battle.players.cards.Card;
-import com.darkgran.farstar.util.DeltaCounter;
+import com.darkgran.farstar.util.SimpleCounter;
+import com.darkgran.farstar.util.SimpleVector2;
 
 public abstract class TokenZoom extends PrintToken {
-    private final DeltaCounter counter = new DeltaCounter(false, 10, 0);
+    private final SimpleCounter counter = new SimpleCounter(false, 30, 0);
+    private boolean activated;
+    private boolean visible;
 
     public TokenZoom(Card card, float x, float y, BattleStage battleStage, CardListMenu cardListMenu) {
         super(card, x, y, battleStage, cardListMenu);
     }
 
     public void update(float delta) {
-        counter.update(delta);
+        counter.update();
+        if (activated && !counter.isEnabled()) {
+            activated = false;
+            visible = true;
+        }
+    }
+
+    @Override
+    public void enable(Card card, TokenType targetType, SimpleVector2 targetXY) {
+        if (getCard() != card) {
+            if (targetType == TokenType.HAND || targetType == TokenType.YARD) {
+                visible = true;
+            } else {
+                activated = true;
+                counter.setEnabled(true);
+            }
+        }
+        super.enable(card, targetType, targetXY);
+    }
+
+    @Override
+    public void disable() {
+        visible = false;
+        super.disable();
     }
 
     @Override
     public void draw(Batch batch) {
-        if (getCard() != null && getTargetType() != null && getTargetXY() != null) {
+        if (visible && getCard() != null && getTargetType() != null && getTargetXY() != null) {
             super.draw(batch);
         }
     }
