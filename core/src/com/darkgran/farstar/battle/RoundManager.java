2a0661dd40d77aa971aee1382a6b508562156482
Dark-Gran <filip.randak@gmail.com>
2021-07-10 00:17:18 +0200

    ships no longer save to junkpile on deploy (= save on death)

--- core/src/com/darkgran/farstar/battle/RoundManager.java
+++ core/src/com/darkgran/farstar/battle/RoundManager.java
@@ -186,7 +186,7 @@ public class RoundManager {
                             battle.getCombatManager().getDuelManager().saveTactic(token.getCard(), targetCard);
                         }
                         if (payPrice) { whoseTurn.payday(token.getCard()); }
-                        if (!(token instanceof FakeToken)) { token.addCardToJunk(); }
+                        if (!(token instanceof FakeToken) && !CardType.isShip(cardType)) { token.addCardToJunk(); }
                         possibilityAdvisor.refresh(battle.getWhoseTurn(), battle);
                     } else if (dropTarget instanceof JunkButton && token instanceof HandToken) { //Target: Discard
                         Junkpile junkpile = ((JunkButton) dropTarget).getPlayer().getJunkpile();
