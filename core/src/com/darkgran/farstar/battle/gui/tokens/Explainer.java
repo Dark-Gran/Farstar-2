b8bae96b6756ef1934dadc25544a8467daed05f6
Dark-Gran <filip.randak@gmail.com>
2021-07-11 13:56:13 +0200

    default explainer separated from TokenZoom

--- core/src/com/darkgran/farstar/battle/gui/tokens/Explainer.java
+++ core/src/com/darkgran/farstar/battle/gui/tokens/Explainer.java
@@ -1,17 +1,33 @@
 package com.darkgran.farstar.battle.gui.tokens;
 
 import com.badlogic.gdx.graphics.Color;
+import com.darkgran.farstar.ColorPalette;
+import com.darkgran.farstar.battle.players.cards.Card;
 import com.darkgran.farstar.gui.TextInTheBox;
 
 public class Explainer extends TextInTheBox {
-    public Explainer(Color fontColor, Color boxColor, String fontPath, String text, boolean wrap, float wrapWidth) {
+
+    public Explainer(Color fontColor, Color boxColor, String fontPath, String text) {
         super(fontColor, boxColor, fontPath, text);
-        setWrapWidth(wrapWidth);
-        setWrap(wrap);
+    }
+
+    public Explainer() {
+        super(ColorPalette.LIGHT, ColorPalette.changeAlpha(ColorPalette.DARK, 0.5f), "fonts/bahnschrift30.fnt", "");
+        setWrapWidth(200f);
+        setWrap(true);
     }
 
     public void setPosition(float x, float y) {
         setX(x+TokenType.PRINT.getWidth()*1.1f);
         setY(y+TokenType.PRINT.getHeight()*0.95f);
     }
+
+    public void refreshText(Card card) {
+        setText(getExplanation(card));
+    }
+
+    protected String getExplanation(Card card) {
+        return "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    }
+
 }
--- core/src/com/darkgran/farstar/battle/gui/tokens/TokenZoom.java
+++ core/src/com/darkgran/farstar/battle/gui/tokens/TokenZoom.java
@@ -1,7 +1,6 @@
 package com.darkgran.farstar.battle.gui.tokens;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.darkgran.farstar.ColorPalette;
 import com.darkgran.farstar.battle.gui.BattleStage;
 import com.darkgran.farstar.battle.gui.CardListMenu;
 import com.darkgran.farstar.battle.players.cards.Card;
@@ -13,14 +12,7 @@ public abstract class TokenZoom extends PrintToken {
     private boolean enabled = true;
     private boolean counting;
     private boolean hidden = true;
-    private Explainer explainer = new Explainer(
-            ColorPalette.LIGHT,
-            ColorPalette.changeAlpha(ColorPalette.DARK, 0.5f),
-            "fonts/bahnschrift30.fnt",
-            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            true,
-            200f
-    );
+    private Explainer explainer = new Explainer();
 
     public TokenZoom(Card card, float x, float y, BattleStage battleStage, CardListMenu cardListMenu, int counterCap) {
         super(card, x, y, battleStage, cardListMenu, false);
@@ -55,6 +47,7 @@ public abstract class TokenZoom extends PrintToken {
     @Override
     public void enable(Card card, TokenType targetType, SimpleVector2 targetXY) {
         if (getCard() != card) {
+            explainer.refreshText(card);
             hidden = false;
             super.enable(card, targetType, targetXY);
         }
