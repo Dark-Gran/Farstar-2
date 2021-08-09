3597732500db6ba937aa754473cda62a08019c77
Dark-Gran <filip.randak@gmail.com>
2021-08-09 19:57:00 +0200

    changed fs rules and therefore reworked duel handling (again; however this should be final gameplay now (until tested more))

--- core/src/com/darkgran/farstar/battle/DuelManager.java
+++ core/src/com/darkgran/farstar/battle/DuelManager.java
@@ -15,7 +15,6 @@ import java.util.*;
 
 public class DuelManager implements Delayer {
     private final float duelDelay = 0.2f;
-    private final float nextTargetDelay = 0.4f;
     private final float simDelay = 0.1f;
 
     public static class AttackInfo implements Comparable<AttackInfo> {
@@ -45,7 +44,6 @@ public class DuelManager implements Delayer {
         public byte getState() { return state; }
         public void setState(byte state) { this.state = state; }
 
-        //comparison used by Old Loop only (see iterateDuels())
         @Override
         public int compareTo(@NotNull DuelManager.AttackInfo o) {
             boolean thisTopStrike = upperStrike != null;
@@ -82,10 +80,8 @@ public class DuelManager implements Delayer {
     private CombatManager combatManager;
     private boolean active = false;
     private Iterator<Map.Entry<FleetToken, AttackInfo>> it;
-    private TreeMap<FleetToken, AttackInfo> duels;
-    private final ArrayList<FleetToken> shooters = new ArrayList<>();
-    private Iterator<FleetToken> shootIt;
-    private Iterator<Token> opIt;
+    TreeMap<FleetToken, AttackInfo> duels;
+    private byte phase; //0. Top-Strikes 1. First-Strikes 2. Regular
 
     //used by Old Loop (see iterateDuels())
     static <K extends Comparable<? super K>,V extends Comparable<? super V>> SortedSet<Map.Entry<K,V>> entriesSortedByValuesThenKeys(Map<K,V> map) {
@@ -105,49 +101,15 @@ public class DuelManager implements Delayer {
     void launchDuels(CombatManager combatManager, TreeMap<FleetToken, AttackInfo> duels, ShotManager shotManager) {
         this.shotManager = shotManager;
         this.duels = duels;
-        shooters.clear();
         if (duels != null && duels.size() > 0) {
             this.combatManager = combatManager;
             this.active = true;
             System.out.println("Sorting and Launching Duels.");
-            //Set<Map.Entry<FleetToken, AttackInfo>> duelSet = entriesSortedByValuesThenKeys(duels); //Old Loop ini (see iterateDuels())
+            //Set<Map.Entry<FleetToken, AttackInfo>> duelSet = entriesSortedByValuesThenKeys(duels);
             //it = duelSet.iterator();
+            phase = 0;
             it = duels.entrySet().iterator();
-            //delayAction(this::iterateDuels, combatManager.getBattleStage().getBattleScreen().getBattleType() == BattleType.SIMULATION ? simDelay : duelDelay*2f);
-            //New Loop
-            while (it.hasNext()) { //Keys (attackers) first
-                Map.Entry<FleetToken, AttackInfo> duel = it.next();
-                if (!shooters.contains(duel.getKey())) {
-                    shooters.add(duel.getKey());
-                }
-            }
-            it = duels.entrySet().iterator();
-            while (it.hasNext()) { //Defenders second
-                Map.Entry<FleetToken, AttackInfo> duel = it.next();
-                if (duel.getValue().getDefender() instanceof FleetToken && !shooters.contains((FleetToken) duel.getValue().getDefender())) {
-                    shooters.add((FleetToken) duel.getValue().getDefender());
-                }
-            }
-            //Sort based on first strikes etc
-            shooters.sort((t1, t2) -> {
-                Map.Entry<FleetToken, AttackInfo> duel1 = CombatManager.getDuel(t1, duels);
-                Map.Entry<FleetToken, AttackInfo> duel2 = CombatManager.getDuel(t2, duels);
-                boolean top1 = duel1.getValue().getUpperStrike() == t1.getCard();
-                boolean top2 = duel2.getValue().getUpperStrike() == t2.getCard();
-                boolean fs1 = AbilityManager.hasAttribute(t1.getCard(), EffectType.FIRST_STRIKE);
-                boolean fs2 = AbilityManager.hasAttribute(t2.getCard(), EffectType.FIRST_STRIKE);
-                if (top1 == top2 && fs1 == fs2) {
-                    return t1.compareTo(t2);
-                } else {
-                    if (top1 || top2) {
-                        return Boolean.compare(top2, top1);
-                    } else {
-                        return Boolean.compare(fs2, fs1);
-                    }
-                }
-            });
-            shootIt = shooters.iterator();
-            delayAction(this::iterateShooters, combatManager.getBattleStage().getBattleScreen().getBattleType() == BattleType.SIMULATION ? simDelay : duelDelay*2f);
+            delayAction(this::iterateDuels, combatManager.getBattleStage().getBattleScreen().getBattleType() == BattleType.SIMULATION ? simDelay : duelDelay);
         } else {
             System.out.println("Invalid number of duels to launch (0 or null).");
             delayAction(this::afterDuels, duelDelay);
@@ -159,97 +121,67 @@ public class DuelManager implements Delayer {
         combatManager.afterDuels();
     }
 
-    /**
-     * OLD LOOP
-     * - iterates through entire Duels instead of individual Shooters (see iterateShooters()): important for First Strike and similar (a matter of gameplay preferences)
-     * - will be reused in the future or become Deprecated
-     */
     private void iterateDuels() {
         if (it != null && it.hasNext()) {
             Map.Entry<FleetToken, AttackInfo> duel = it.next();
             combatManager.setDuelState(duel, (byte) 1);
-            delayAction(() -> performDuel(duel), combatManager.getBattleStage().getBattleScreen().getBattleType() == BattleType.SIMULATION ? simDelay : duelDelay);
+            delayAction(() -> performDuel(duel, phase), combatManager.getBattleStage().getBattleScreen().getBattleType() == BattleType.SIMULATION ? simDelay : duelDelay);
         } else {
-            //afterDuels();
-            delayAction(this::afterDuels, combatManager.getBattleStage().getBattleScreen().getBattleType() == BattleType.SIMULATION ? simDelay : duelDelay*2f);
+            if (phase >= 2) {
+                //afterDuels();
+                delayAction(this::afterDuels, combatManager.getBattleStage().getBattleScreen().getBattleType() == BattleType.SIMULATION ? simDelay : duelDelay*2f);
+            } else {
+                phase++;
+                it = duels.entrySet().iterator();
+                iterateDuels();
+            }
         }
     }
 
-    private void performDuel(Map.Entry<FleetToken, AttackInfo> duel) {
-        exeBothSides(duel.getKey().getCard(), duel.getValue());
-        combatManager.setDuelState(duel, (byte) 2);
-        delayAction(this::iterateDuels, combatManager.getBattleStage().getBattleScreen().getBattleType() == BattleType.SIMULATION ? simDelay : duelDelay);
-    }
-
-    private void exeBothSides(BattleCard att, AttackInfo attackInfo) {
-        BattleCard def = attackInfo.getDefender().getCard();
+    private void performDuel(Map.Entry<FleetToken, AttackInfo> duel, byte phase) {
+        BattleCard att = duel.getKey().getCard();
+        BattleCard def = duel.getValue().getDefender().getCard();
         boolean attFS = !att.isMS() && AbilityManager.hasAttribute(att, EffectType.FIRST_STRIKE);
         boolean defFS = !def.isMS() && AbilityManager.hasAttribute(def, EffectType.FIRST_STRIKE);
-        boolean attTop = attackInfo.getUpperStrike() == att;
-        boolean defTop = attackInfo.getUpperStrike() == def;
-        if (attTop || defTop) {
-            if (exeOneSide(attTop ? att : def, attTop ? def : att, false)) {
-                exeOneSide(attTop ? def : att, attTop ? att : def, true);
-            }
-        } else if (attFS != defFS) {
-            if (attFS) {
-                if (att.isKilledByFirstStrike() || exeOneSide(att, def, false)) {
-                    if (!def.isKilledByFirstStrike()) { exeOneSide(def, att, true); }
+        boolean attTop = duel.getValue().getUpperStrike() == att;
+        boolean defTop = duel.getValue().getUpperStrike() == def;
+        switch (phase) {
+            case 0:
+                if (attTop) {
+                    if (!exeOneSide(att, def, false)) {
+                        def.setKilledByTopStrike(true);
+                    }
                 }
-            } else {
-                if (def.isKilledByFirstStrike() || exeOneSide(def, att, false)) {
-                    if (!att.isKilledByFirstStrike()) { exeOneSide(att, def, true); }
+                if (defTop) {
+                    if (!exeOneSide(def, att, false)) {
+                        att.setKilledByTopStrike(true);
+                    }
                 }
-            }
-        } else {
-            if (!att.isKilledByFirstStrike()) { exeOneSide(att, def, false); }
-            if (!def.isKilledByFirstStrike()) { exeOneSide(def, att, false); }
+                break;
+            case 1:
+                if (!attTop && attFS && !att.isKilledByTopStrike()) {
+                    if (!exeOneSide(att, def, false)) {
+                        def.setKilledByFirstStrike(true);
+                    }
+                }
+                if (!defTop && defFS && !def.isKilledByTopStrike()) {
+                    if (!exeOneSide(def, att, false)) {
+                        att.setKilledByFirstStrike(true);
+                    }
+                }
+                break;
+            case 2:
+                if (!attTop && !attFS && !att.isKilledByTopStrike() && !att.isKilledByFirstStrike()) {
+                    exeOneSide(att, def, false);
+                }
+                if (!defTop && !defFS && !def.isKilledByTopStrike() && !def.isKilledByFirstStrike()) {
+                    exeOneSide(def, att, false);
+                }
+                break;
         }
-        if (attFS || defFS || attTop || defTop) {
-            if (att.getHealth()<=0) { att.setKilledByFirstStrike(true); }
-            if (def.getHealth()<=0) { def.setKilledByFirstStrike(true); }
-        }
+        iterateDuels();
     }
 
-    /**
-     * NEW LOOP
-     * (iterates through all ships that have an opponent)
-     */
-    private void iterateShooters() {
-        if (duels != null && duels.size() > 0 && shootIt != null && shootIt.hasNext()) {
-            FleetToken shooter = shootIt.next();
-            ArrayList<Token> opponents = CombatManager.getDuelOpponent(shooter, duels);
-            opIt = opponents.iterator();
-            delayAction(() -> iterateAllTargets(shooter), combatManager.getBattleStage().getBattleScreen().getBattleType() == BattleType.SIMULATION ? simDelay : duelDelay);
-        } else {
-            //afterDuels();
-            delayAction(this::afterDuels, combatManager.getBattleStage().getBattleScreen().getBattleType() == BattleType.SIMULATION ? simDelay : nextTargetDelay);
-        }
-    }
-
-    private void iterateAllTargets(FleetToken shooter) {
-        boolean sFS = AbilityManager.hasAttribute(shooter.getCard(), EffectType.FIRST_STRIKE);
-        boolean sTop = CombatManager.getDuel(shooter, duels).getValue().getUpperStrike() == shooter.getCard();
-        if (opIt.hasNext() && ((sTop || !shooter.getCard().isKilledByTopStrike()) && (sFS || !shooter.getCard().isKilledByFirstStrike()))) {
-            Token opponent = opIt.next();
-            shootOneTarget(shooter, opponent.getCard(), sTop, sFS);
-        } else {
-            delayAction(this::iterateShooters, combatManager.getBattleStage().getBattleScreen().getBattleType() == BattleType.SIMULATION ? simDelay : duelDelay);
-        }
-    }
-
-    private void shootOneTarget(FleetToken shooter, BattleCard def, boolean attTop, boolean attFS) {
-        if (!exeOneSide(shooter.getCard(), def, false)) {
-            if (attTop) {
-                def.setKilledByTopStrike(true);
-            } else if (attFS) {
-                def.setKilledByFirstStrike(true);
-            }
-        }
-        delayAction(() -> iterateAllTargets(shooter), combatManager.getBattleStage().getBattleScreen().getBattleType() == BattleType.SIMULATION ? simDelay : duelDelay*1.5f);
-    }
-
-    //Shared (used by both loops)
     private boolean exeOneSide(BattleCard att, BattleCard def, boolean delayedAnimation) { //returns survival
         if (!att.isMS()) {
             int dmg = att.getCardInfo().getOffense();
