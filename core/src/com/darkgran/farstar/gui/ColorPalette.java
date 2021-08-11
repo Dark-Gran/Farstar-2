b2b6900a5c195fdd2bb6e96911a42c5408252bf4
Dark-Gran <filip.randak@gmail.com>
2021-08-11 23:34:42 +0200

    basic Black slightly darker

--- core/src/com/darkgran/farstar/gui/ColorPalette.java
+++ core/src/com/darkgran/farstar/gui/ColorPalette.java
@@ -10,7 +10,7 @@ public final class ColorPalette { //in-future: make into a SuperScreen instance 
     public static final Color ENERGY = new Color(0.243f, 0.749f, 0.918f, 1);
     public static final Color MATTER = new Color(0.702f, 0.4f, 0.176f, 1);
     public static final Color PITCH_BLACK = new Color(0f, 0f, 0f, 1);
-    public static final Color BLACK = new Color(0.06f, 0.06f, 0.07f, 1);
+    public static final Color BLACK = new Color(0.05f, 0.05f, 0.05f, 1);
     public static final Color BLACKISH = new Color(0.07f, 0.07f, 0.07f, 1); //121212
     public static final Color RED = new Color(0.6f, 0.06f, 0.06f, 1); //990f0f
     public static final Color GREEN = new Color(0.066f, 0.6f, 0.066f, 1);
