88790a21d96ba19ccaeb45b0753ee7d145482dfb
Dark-Gran <filip.randak@gmail.com>
2021-08-13 17:24:20 +0200

    Explainer: adjusted box size

--- core/src/com/darkgran/farstar/gui/tokens/Explainer.java
+++ core/src/com/darkgran/farstar/gui/tokens/Explainer.java
@@ -45,14 +45,14 @@ public class Explainer extends TextInTheBox {
         if (textWH.x < getWrapWidth()) {
             x = (x + (getWrapWidth() - textWH.x)/2);
         }
-        flavourOffsetY = (explanation.equals("") ? 0 : textWH.y);
+        flavourOffsetY = (explanation.equals("") ? 0 : textWH.y+10f);
         setupBox(x, y, getWrapWidth()+40f, (textWH.y+flavourWH.y)+40f);
         centralizeBox();
     }
 
     @Override
     public SimpleVector2 boxOriginFromTextCenter(float width, float height, BitmapFont font, String text, float x, float y, float wrapWidth, boolean wrap) {
-        SimpleVector2 textWH = TextDrawer.getTextWH(getFont(), explanation, wrapWidth, wrap);
+        SimpleVector2 textWH = TextDrawer.getTextWH(font, explanation, wrapWidth, wrap);
         SimpleVector2 flavourWH = TextDrawer.getTextWH(AssetLibrary.getInstance().getAssetManager().get(italicPath, BitmapFont.class), flavour, wrapWidth, wrap);
         SimpleVector2 totalWH = new SimpleVector2(Math.max(textWH.x, flavourWH.x), textWH.y+flavourWH.y);
         return new SimpleVector2((x + totalWH.x/2) - width/2,(y - totalWH.y/2) - height/2);
@@ -61,6 +61,10 @@ public class Explainer extends TextInTheBox {
     public void refreshText(BattleCard battleCard) {
         explanation = getMainContent(battleCard);
         flavour = getFlavourContent(battleCard);
+        if (!explanation.equals("") && !flavour.equals("")) {
+            explanation = explanation+"\n";
+            flavour = flavour+"\n";
+        }
         setText(explanation+flavour);
         setupBox();
     }
@@ -80,34 +84,34 @@ public class Explainer extends TextInTheBox {
         } else {
             boolean first = true;
             if (CardType.needsDefense(battleCard.getCardInfo().getCardType()) && battleCard.getDamage() > 0) {
-                str.append("Current/Max. Shields: ").append(battleCard.getHealth()).append("/").append(battleCard.getCardInfo().getDefense()).append("                      \n");
+                str.append("Current/Max. Shields: ").append(battleCard.getHealth()).append("/").append(battleCard.getCardInfo().getDefense()).append("                      ");
                 first = false;
             }
             String shipOrSupport = battleCard.getCardInfo().getCardType() == CardType.SUPPORT ? "support" : "ship";
             if (battleCard.isUsed()) {
                 if (!first) {
-                    str.append("\n");
+                    str.append("\n\n");
                 } else {
                     first = false;
                 }
-                str.append("Disabled:\nThis ").append(shipOrSupport).append(" cannot attack or use abilities until next Turn.\n");
+                str.append("Disabled:\nThis ").append(shipOrSupport).append(" cannot attack or use abilities until next Turn.");
             }
             boolean tactic = battleCard.getCardInfo().getCardType() == CardType.TACTIC; // || AbilityManager.hasStarter(battleCard, AbilityStarter.TACTIC)
             if (tactic) {
                 if (!first) {
-                    str.append("\n");
+                    str.append("\n\n");
                 } else {
                     first = false;
                 }
-                str.append("Tactic:\nTactics may be deployed both in Your Turn and in the Tactical Phase.\nUsually the best choice is to keep them for the Tactical Phase.\n");
+                str.append("Tactic:\nTactics may be deployed both in Your Turn and in the Tactical Phase.\nUsually the best choice is to keep them for the Tactical Phase.");
             }
             if (battleCard.getCardInfo().getCardType() == CardType.SUPPORT) {
                 if (!first) {
-                    str.append("\n");
+                    str.append("\n\n");
                 } else {
                     first = false;
                 }
-                str.append("Support:\nYou can deploy Supports next to Your Mothership.\n");
+                str.append("Support:\nYou can deploy Supports next to Your Mothership.");
             }
             boolean FSpresent = false;
             boolean USEpresent = false;
@@ -116,27 +120,27 @@ public class Explainer extends TextInTheBox {
                     switch (effect.getEffectType()) {
                         case GUARD:
                             if (!first) {
-                                str.append("\n");
+                                str.append("\n\n");
                             } else {
                                 first = false;
                             }
-                            str.append("Guard:\nAttackers without Reach must always target Guards first.\n");
+                            str.append("Guard:\nAttackers without Reach must always target Guards first.");
                             break;
                         case REACH:
                             if (!first) {
-                                str.append("\n");
+                                str.append("\n\n");
                             } else {
                                 first = false;
                             }
-                            str.append("Reach:\nAllows targeting over Guards, but only if the Reach is same as (or greater than) number of Guards.\n");
+                            str.append("Reach:\nAllows targeting over Guards, but only if the Reach is same as (or greater than) number of Guards.");
                             break;
                         case ARMOR:
                             if (!first) {
-                                str.append("\n");
+                                str.append("\n\n");
                             } else {
                                 first = false;
                             }
-                            str.append("Armor:\nReduces all incoming damage.\n");
+                            str.append("Armor:\nReduces all incoming damage.");
                             break;
                         case FIRST_STRIKE:
                             FSpresent = true;
@@ -157,21 +161,21 @@ public class Explainer extends TextInTheBox {
             }
             if (USEpresent) {
                 if (!first) {
-                    str.append("\n");
+                    str.append("\n\n");
                 } else {
                     first = false;
                 }
-                str.append("Usable Ability:\nMay be used only once per Turn and only in Your Turn.\n");
+                str.append("Usable Ability:\nMay be used only once per Turn and only in Your Turn.");
             }
             if (!FSpresent) {
                 FSpresent = AbilityManager.upgradesFirstStrike(battleCard);
             }
             if (FSpresent) {
                 if (!first) {
-                    str.append("\n");
+                    str.append("\n\n");
                 }
                 //else { first = false; }
-                str.append("First Strike:\nShips with First Strike always shoot first: if a ship is destroyed by First Strike, it will not retaliate.\n");
+                str.append("First Strike:\nShips with First Strike always shoot first: if a ship is destroyed by First Strike, it will not retaliate.");
             }
         }
         return str.toString();
